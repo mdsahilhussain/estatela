87578bb0ec5f834aa3edf3c2be7cd0afd610f073
mdsahilhussain <mdsahil.hussain7@gmail.com>
2026-04-25 14:45:13 +0530

    Add custom fonts and update layout; enhance onboarding and home screens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,12 +9,12 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-primary">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-sans-extrabold text-primary">
+        Home
       </Text>
 
       <Link href="/onboarding" className="mt-4 rounded bg-primary px-4 py-2">
-        Get Started
+        Get to onboarding
       </Link>
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary px-4 py-2">
         Sign In
